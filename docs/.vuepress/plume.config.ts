400b8b16058257d16891a4898997c9d85b99425f
LeafS825 <871850079@qq.com>
2026-01-29 18:19:59 +0800

    更新页脚

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -65,7 +65,7 @@ export default defineThemeConfig({
       collections: zhcollections,
       footer: {
         message: '由 <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a> 驱动',
-        copyright: `© 2025-${currentYear} 版权所有 <a href="https://sectl.top>SECTL</a> | 采用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> 许可协议<img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/by.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg">`,
+        copyright: `© 2025-${currentYear} 版权所有 <a href="https://sectl.top>SECTL</a> | 采用 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> 许可协议<img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/by.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"><a href="https://icp.gov.moe/?keyword=20260108" target="_blank">萌ICP备20260108号</a>`,
       },
     },
     '/en/': {
@@ -73,7 +73,7 @@ export default defineThemeConfig({
       collections: encollections,
       footer: {
         message: 'Powered by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
-        copyright: `© 2025-${currentYear} by <a href="https://sectl.top>SECTL</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/by.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg">`,
+        copyright: `© 2025-${currentYear} by <a href="https://sectl.top>SECTL</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/by.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"><img class="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"><a href="https://icp.gov.moe/?keyword=20260108" target="_blank">萌ICP备20260108号</a>`,
       },
     },
   },
